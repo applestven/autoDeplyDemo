7c546feac776609444143b765109e3b5e7565fda
applestven <applestven@gmail.com.com>
2023-04-23 21:20:24 +0800

    测试是否更新

--- vite-Veu/src/components/HelloWorld.vue
+++ vite-Veu/src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>hello  gitAction</p>
 </template>
 
 <script>
