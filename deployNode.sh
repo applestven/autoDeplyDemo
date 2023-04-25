dae090d0034c47ef8eea256b5aceb214dee8c959
applestven <applestven@gmail.com.com>
2023-04-25 21:00:23 +0800

    重写了sh文件

--- deployNode.sh
+++ deployNode.sh
@@ -1,25 +1,18 @@
-## 放在linux服务器上的文件   applestven是dockerHub用户名  $1是需要填的密码
-echo -e "---------docker Login--------"
-docker login --username=$1  --password=$2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
-echo -e "---------docker Stop--------"
-docker stop nodejstest 
-echo -e "---------docker Rm--------"
+#!/bin/bash
+
+# 定义变量
+IMAGE_NAME="applestven/nodejstest"
+TAG_NAME="latest"
+
+# 停止并删除容器
+docker stop nodejstest
 docker rm nodejstest
-docker rmi applestven/nodejstest:latest
-echo -e "---------docker Pull--------"
-docker pull applestven/nodejstest:latest
-echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 8181:3000 --name nodejstest applestven/nodejstest:latest
-echo -e "---------deploy Success--------"
-# echo -e "---------docker Login--------"
-# docker login --username=$1  --password=$2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
-# echo -e "---------docker Stop--------"
-# docker stop <你镜像的别名(在下面 会生成)>  # 注意修改
-# echo -e "---------docker Rm--------"
-# docker rm <你容器名字> 									 # 注意修改
-# docker rmi <你镜像的名字>									# 注意修改
-# echo -e "---------docker Pull--------"
-# docker pull <docker服务器上镜像的名字>:<镜像的tag默认是github分支的名字>  		 # 注意修改
-# echo -e "---------docker Create and Start--------"
-# docker run --rm -d -p 80:80 --name <容器的名字> <docker服务器上镜像的名字>:<镜像的tag默认是github分支的名字> # 注意修改
-# echo -e "---------deploy Success--------" 
+
+# 删除旧镜像
+docker rmi ${IMAGE_NAME}:${TAG_NAME}
+
+# 从 Docker Hub 下载最新镜像
+docker pull ${IMAGE_NAME}:${TAG_NAME}
+
+# 创建并启动新容器
+docker run -d -p 8181:3000 --name nodejstest ${IMAGE_NAME}:${TAG_NAME}
