4c3a2fa105881d1ed78ecaabe25b6febad7d438c
applestven <applestven@gmail.com.com>
2023-04-25 21:16:58 +0800

    重写了sh文件1

--- deployNode.sh
+++ deployNode.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-
 # 定义变量
 IMAGE_NAME="applestven/nodejstest"
 TAG_NAME="latest"
-
 # 停止并删除容器
 docker stop nodejstest
 docker rm nodejstest
-
 # 删除旧镜像
 docker rmi ${IMAGE_NAME}:${TAG_NAME}
-
 # 从 Docker Hub 下载最新镜像
-docker pull ${IMAGE_NAME}:${TAG_NAME}
-
+docker pull applestven/nodejstest:latest
 # 创建并启动新容器
 docker run -d -p 8181:3000 --name nodejstest ${IMAGE_NAME}:${TAG_NAME}
