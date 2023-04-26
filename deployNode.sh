7c3f3b85e0727ed8ab78eb456c56dc24091a6574
applestven <applestven@gmail.com.com>
2023-04-26 15:02:04 +0800

    ccc

--- deployNode.sh
+++ deployNode.sh
@@ -2,11 +2,11 @@
 echo "---------docker Login--------"
 echo $2 | docker login --username $1 --password-stdin
 echo "---------docker Stop--------"
-docker stop nodejstest
+docker stop demofor-node
 echo "---------docker Rm--------"
-docker rm nodejstest
+docker rm demofor-node
 echo "---------docker Pull--------"
-docker pull applestven/nodejstest:latest
+docker pull applestven/demofor-node:latest
 echo "---------docker Create and Start--------"
-docker run -d -p 8282:3000 --name nodejstest applestven/nodejstest:latest
+docker run -d -p 8282:3000 --name demofor-node applestven/demofor-node:latest
 echo "---------deploy Success--------"
