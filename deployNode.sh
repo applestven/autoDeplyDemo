87ca5d7d58f1364807d4ae5099010ea87dd6308d
applestven <applestven@gmail.com.com>
2023-04-26 10:23:39 +0800

    1111111111111111111111

--- deployNode.sh
+++ deployNode.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 echo "---------docker Login--------"
 echo $2 | docker login --username $1 --password-stdin
 echo "---------docker Stop--------"
 docker stop nodejstest
 echo "---------docker Rm--------"
 docker rm nodejstest
-docker rmi applestven/nodejstest:latest
 echo "---------docker Pull--------"
 docker pull applestven/nodejstest:latest
 echo "---------docker Create and Start--------"
