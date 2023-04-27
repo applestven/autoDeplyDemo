de99a2906c0a6dbd211b3924f47924242c734b72
applestven <applestven@gmail.com.com>
2023-04-27 10:39:51 +0800

    33333333333333

--- demoforNode.sh
+++ demoforNode.sh
@@ -2,4 +2,4 @@ docker stop nodejstest
 docker rm nodejstest
 docker pull applestven/nodejstest:latest
 # docker run -d -p 8282:3000 --name nodejstest applestven/nodejstest:latest sh -c "npm i && pm2 start /service/koa/src/main.js --no-daemon"
-docker run -d -p 8282:3000 --name nodejstest applestven/nodejstest:latest pm2-runtime /service/koa/src/main
+docker run -d -p 8282:3000 --name nodejstest applestven/nodejstest:latest
