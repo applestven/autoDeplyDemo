326acc3734f92fa7a922cad268cac769a81cf90e
applestven <applestven@gmail.com.com>
2023-04-27 12:26:42 +0800

    测试前后端一起部署

---
+++ vite-Veu/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  测试前后端部署
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
